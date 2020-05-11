45216a320b983fc6587fa40fe38a955cbced66d8
Ruben <atosc08@gmail.com>
2020-05-11 06:14:07 +0200

    Routing ready and data

--- examples/server.ts
+++ examples/server.ts
@@ -1,5 +1,8 @@
 import { Transport } from "../transport.ts";
-
 let transport = new Transport(8080, () => {
   console.log("Test");
+});
+
+transport.on("test", (socket: any, msg: any) => {
+  console.log(msg);
 });
--- transport.ts
+++ transport.ts
@@ -6,7 +6,7 @@ import {
 } from "https://deno.land/std/ws/mod.ts";
 
 interface Route {
-  name: String;
+  route: String;
   cb: Function;
 }
 
@@ -21,9 +21,29 @@ export class Transport {
   }
   private async start(port: number) {
     for await (const req of serve({ port: port })) {
+      const { conn, r: bufReader, w: bufWriter, headers } = req;
+      const sock = await acceptWebSocket({
+        conn,
+        bufReader,
+        bufWriter,
+        headers,
+      });
+      let connection = this.route.find((data) => data.route === "connection");
+      if (connection) {
+        connection.cb();
+      }
+      for await (const ev of sock) {
+        if (typeof ev === "string") {
+          let eventObject = JSON.parse(ev);
+          let event = this.route.find(
+            (data) => data.route === eventObject.route
+          );
+          event?.cb(sock, eventObject.data);
+        }
+      }
     }
   }
   on(route: String, cb: Function) {
-    this.route.push({ name: route, cb: cb });
+    this.route.push({ route: route, cb: cb });
   }
 }
